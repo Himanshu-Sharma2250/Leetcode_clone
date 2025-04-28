9a37a60d033682b32d9660f0e395d013060f703c
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-04-28 15:57:44 +0530

    add create problem controller and judge0 lib

---
+++ backend/src/controllers/problem.controller.js
@@ -0,0 +1,104 @@
+import { db } from "../libs/db.js";
+import { getJudge0LanguageId, pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
+
+
+export const createProblem = async (req, res) => {
+    // get all the data from the req.body
+    // check user role again
+    // loop through each and every reference sol
+
+    const {title, description, difficulty, tags, examples, constraints, testcases, codeSnippets, referenceSolutions} = req.body;
+
+    if (req.user.role !== "ADMIN") {
+        return res.status(403).json({
+            message: "You are not allowed to create a problem"
+        })
+    }
+
+    try {
+        for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+            const languageId = getJudge0LanguageId(language);
+
+            if (!languageId) {
+                return res.status(400).json({
+                    error: `Language ${language} id not supported`
+                })
+            }
+
+            const submissions = testcases.map(({input, output}) => ({
+                source_code:solutionCode,
+                language_id:languageId,
+                stdin:input,
+                expected_output:output,
+            }))
+
+            const submissionResults = await submitBatch(submissions);
+
+            const tokens = submissionResults.map((res) => res.token);
+
+            const results = await pollBatchResults(tokens);
+
+            for (let i = 0; i < results.length; i++) {
+                const result = results[i];
+
+                console.log("Result-----", result);
+                // console.log(
+                //   `Testcase ${i + 1} and Language ${language} ----- result ${JSON.stringify(result.status.description)}`
+                // );
+
+                if (result.status.id !== 3) {
+                    return res.status(400).json({
+                        error: `Testcase ${i+1} failed for language ${language}`
+                    })
+                }
+            }
+
+            const newProblem = await db.problem.create({
+                data: {
+                    title,
+                    description,
+                    difficulty,
+                    tags,
+                    examples,
+                    constraints,
+                    testcases,
+                    codeSnippets,
+                    referenceSolutions,
+                    userId: req.user.id,
+                }
+            })
+
+            return res.status(201).json({
+                success: true,
+                message: "Problem created Successfully",
+                problem: newProblem
+            })
+        }
+    } catch (error) {
+        console.error("Error creating Problem ", error)
+        return res.status(500).json({
+            success: false,
+            error: "Error while creating problem"
+        })
+    }
+}
+
+export const getAllProblems = async (req, res) => {
+
+}
+
+export const getProblemByID = async (req, res) => {
+
+}
+
+export const updateProblem = async (req, res) => {
+
+}
+
+export const deleteProblem = async (req, res) => {
+
+}
+
+export const getAllProblemsSolvedByUser = async (req, res) => {
+
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js";
 
 dotenv.config({
     path:"../.env"
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 8080;
 
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is listening at port : 8080")
